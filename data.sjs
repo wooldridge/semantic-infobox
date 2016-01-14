1d568b4568c752cc00c5d53d7f9307893fa46eec
Mike Wooldridge <mike@hellion.org>
2016-01-14 12:31:08 -0800

    First commit

---
+++ data.sjs
@@ -0,0 +1,44 @@
+var sem = require('/MarkLogic/semantics.xqy');
+
+// Format search query from HTTP request (example: "Titanic"@en)
+var str = '"' + xdmp.getRequestField('query') + '"@en';
+
+// Build SPARQL query
+var query = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> ' +
+            'CONSTRUCT { ?subj ?pred ?obj } ' +
+            'WHERE ' +
+            '{ ' +
+            '  ?subj foaf:name ' + str + ' . ' +
+            '?subj ?pred ?obj . ' +
+            '}';
+
+// Predicates to search for
+var map = {}
+map['http://xmlns.com/foaf/0.1/name'] = 'name';
+map['http://dbpedia.org/ontology/abstract'] = 'description';
+map['http://dbpedia.org/ontology/thumbnail'] = 'thumbnail';
+map['http://dbpedia.org/ontology/Work/runtime'] = 'runtime';
+map['http://dbpedia.org/ontology/gross'] = 'gross';
+map['http://dbpedia.org/ontology/budget'] = 'budget';
+
+var itr = sem.sparql(query);
+
+var result = {};
+
+// Cycle through results
+for (var item of itr) {
+  var itemObj = item.toObject();
+  var pred = itemObj.triple.predicate;
+  var obj = itemObj.triple.object;
+  // If predicate matches, store in results
+  if (map[pred]) {
+    // Can be an object or string
+    if (obj.value) {
+      result[map[pred]] = obj.value;
+    } else {
+      result[map[pred]] = obj;
+    }
+  }
+}
+
+result;
---
+++ script.js
@@ -0,0 +1,64 @@
+function formatSnippet(matches) {
+  var result = '<div class="snippet">';
+  for (var match of matches) {
+    for (var m of match['match-text']) {
+      if (m.highlight) {
+        result += '<span class="highlight">' + m.highlight + '</span> ';
+      } else {
+        result += m + ' ';
+      }
+    }
+    result += '... ';
+  }
+  result += '</div>';
+  return result;
+}
+
+$('#submit').click(function(event) {
+  event.preventDefault();
+  var str = $('#query').val();
+  // Dismiss any previous results
+  $('#summary').html('');
+  $('#results').html('');
+  $('#infobox').html('').hide();
+  $('#spinner').show();
+  var results = '';
+  var results2 = '';
+  // Get search results
+  var url = '/v1/search?format=json&view=all&options=all&start=1&pageLength=10&q=' + str;
+  $.get(url, function(data) {
+    $('#spinner').hide();
+    $('#summary').html('Results found: ' + data.total);
+    if (data.total > 0) {
+      var first = data.results[0].metadata[8].film;
+      for (var res of data.results) {
+        results += '<div class="result">';
+        results += '<div class="result-title">';
+        // Actor vs. Film
+        if (res.metadata[7]) {
+          results += res.metadata[7].name + ' - "' + res.metadata[8].film + '"';
+        } else {
+          results += '"' + res.metadata[6].film + '"';
+        }
+        results += '</div>';
+        results += '<div class="result-uri">' + res.uri + '</div>';
+        results += formatSnippet(res.matches);
+        results += '</div>';
+      }
+      $('#results').html(results);
+      // Get spotlight result
+      var url2 = '/semantic-infobox/data.sjs?query=' + first;
+      $.get(url2, function(data2) {
+        if (data2.name) {
+          $('#infobox').show();
+          results2 += '<div class="infobox-heading">Spotlight</div>';
+          results2 += '<div class="infobox-title">' + data2.name + '</div>';
+          var desc = $.trim(data2.description).substring(0, 300)
+            .split(' ').slice(0, -1).join(' ') + '...';
+          results2 += '<div class="infobox-desc">' + desc + '</div>';
+          $('#infobox').html(results2);
+        }
+      });
+    }
+  });
+});
