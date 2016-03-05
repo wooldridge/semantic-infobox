7f8a3d05ecf7eb244df09e628617794c683750c0
Mike Wooldridge <mike@hellion.org>
2016-03-04 18:02:56 -0800

    Add SPARQL endpoint

--- server.js
+++ server.js
@@ -20,11 +20,10 @@ var auth = {
   sendImmediately: false
 };
 
-// /v1/search GET
+// /v1/search POST
 app.get('/search', function(req, res){
-  console.log('start');
   var q = req.query.q;
-  var url = 'http://localhost:8000/v1/search';
+  var url = 'http://localhost:8000/v1/search?options=infobox';
   var body = {
     "query": {
       "queries": [{
@@ -44,7 +43,6 @@ app.get('/search', function(req, res){
       }]
     }
   };
-
   request({
     method: "POST",
     url: url,
@@ -55,7 +53,6 @@ app.get('/search', function(req, res){
     },
     auth: auth
   }, function (error, response, body) {
-    console.log('ok');
     if (response) {
       if ((response.statusCode >= 200) && (response.statusCode < 300)) {
         res.set('Content-Type', 'application/json');
@@ -66,7 +63,87 @@ app.get('/search', function(req, res){
         // if not, send response (which includes ML-generated URI in location)
         } else {
           console.log(JSON.stringify(response.headers));
-          //res.send(response.headers);
+          res.send(response);
+        }
+      } else {
+        console.log('Error: '+ response.statusCode);
+        console.log(body);
+        res.status(response.statusCode).send();
+      }
+    } else {
+      console.log('Error: No response object');
+    }
+  });
+});
+
+// Predicates to search for
+var map = {}
+map['http://xmlns.com/foaf/0.1/name'] = 'name';
+map['http://dbpedia.org/ontology/abstract'] = 'description';
+map['http://dbpedia.org/ontology/thumbnail'] = 'thumbnail';
+map['http://dbpedia.org/ontology/Work/runtime'] = 'runtime';
+map['http://dbpedia.org/ontology/gross'] = 'gross';
+map['http://dbpedia.org/ontology/budget'] = 'budget';
+
+function processResults(items) {
+  var result = {};
+  // Cycle through results
+  for (var subj in items) {
+    for (var pred in items[subj]) {
+      var obj = items[subj][pred][0];
+      // If predicate matches, store in results
+      if (map[pred]) {
+        // Can be an object or string
+        if (obj.value) {
+          result[map[pred]] = obj.value;
+        } else {
+          result[map[pred]] = obj;
+        }
+      }
+      // Handle 8.0-3 API (value props)
+      if (map[pred.value]) {
+        if (obj.value) {
+          result[map[pred.value]] = obj.value;
+        } else {
+          result[map[pred.value]] = obj;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+// /v1/graphs/sparql POST
+app.get('/sparql', function(req, res){
+  var url = 'http://localhost:8000/v1/graphs/sparql';
+  var q = '"' + req.query.q + '"@en';
+  var body = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> ' +
+              'CONSTRUCT { ?subj ?pred ?obj } ' +
+              'WHERE ' +
+              '{ ' +
+              '  ?subj foaf:name ' + q + ' . ' +
+              '?subj ?pred ?obj . ' +
+              '}';
+  request({
+    method: "POST",
+    url: url,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: auth,
+    body: body
+  }, function (error, response, body) {
+    if (response) {
+      if ((response.statusCode >= 200) && (response.statusCode < 300)) {
+        res.set('Content-Type', 'application/json');
+        // response may or may not have body
+        if(body) {
+          var results = processResults(body);
+          res.send(results);
+        // if not, send response (which includes ML-generated URI in location)
+        } else {
+          console.log(JSON.stringify(response.headers));
           res.send(response);
         }
       } else {
